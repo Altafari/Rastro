946f91da9e0061ccd9c95483100bb7c219e7af24
mws <mws@mail.com>
2020-05-10 05:22:55 +0300

    UI tweaked for low res display

--- src/rastro/ui/MainDialog.java
+++ src/rastro/ui/MainDialog.java
@@ -18,17 +18,23 @@ public class MainDialog {
 
     private static void addComponentsToPane(Container pane) {
         SystemManager sysMgr = SystemManager.getInstance();
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.PAGE_AXIS));
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.PAGE_AXIS));
+        leftPanel.add(sysMgr.getGrblCommPanel());
+        leftPanel.add(sysMgr.getRastroCommPanel());
+        leftPanel.add(sysMgr.getImageControlPanel());
+        leftPanel.add(sysMgr.getImageInfoPanel());
+        leftPanel.add(Box.createVerticalGlue());
+        leftPanel.add(sysMgr.getFlipMirroringPanel());
+        rightPanel.add(sysMgr.getCncOriginPanel());
+        rightPanel.add(sysMgr.getCncPositioningPanel());
+        rightPanel.add(sysMgr.getProgramControlPanel());
         JPanel ctrlPanel = new JPanel();
-        ctrlPanel.setLayout(new BoxLayout(ctrlPanel, BoxLayout.PAGE_AXIS));
-        ctrlPanel.add(sysMgr.getGrblCommPanel());
-        ctrlPanel.add(sysMgr.getRastroCommPanel());
-        ctrlPanel.add(sysMgr.getImageControlPanel());
-        ctrlPanel.add(sysMgr.getImageInfoPanel());
-        ctrlPanel.add(sysMgr.getCncOriginPanel());
-        ctrlPanel.add(sysMgr.getCncPositioningPanel());
-        ctrlPanel.add(sysMgr.getProgramControlPanel());
-        ctrlPanel.add(Box.createVerticalGlue());
-        ctrlPanel.add(sysMgr.getFlipMirroringPanel());
+        ctrlPanel.setLayout(new BoxLayout(ctrlPanel, BoxLayout.LINE_AXIS));
+        ctrlPanel.add(leftPanel);
+        ctrlPanel.add(rightPanel);
         pane.add(ctrlPanel, BorderLayout.LINE_START);        
         pane.add(new JPanel(), BorderLayout.LINE_END);
     }
